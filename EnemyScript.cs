81b75365d80eee4de2f0efaae57f310db39c5399
ShironeTRINOLINE <65526604+ShironeTRINOLINE@users.noreply.github.com>
2020-05-20 20:10:42 +0900

    Add files via upload

---
+++ EnemyScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyScript : MonoBehaviour
+{
+    //public Transform target;
+
+    public GameObject target;
+    private NavMeshAgent agent;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.LookAt(target);
+
+        agent.destination = target.transform.position;
+
+
+
+    }
+}
